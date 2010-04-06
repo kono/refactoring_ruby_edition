ef1543f68d86dbf2678c8a4897fabc978eafdcd6
Hiroshi Kono <hiroshi.kono@gmail.com>
2010-04-06 21:35:30 +0900

    p.37 moved function "charge" from Customer class to Rental class.

--- chapter1/Customer.rb
+++ chapter1/Customer.rb
@@ -37,16 +37,6 @@ class Customer
   end
   
   def amount_for(rental)
-    result = 0
-    case rental.movie.price_code
-      when Movie::REGULAR
-        result += 2
-        result += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
-      when Movie::NEW_RELEASE
-        result += rental.days_rented * 3
-      when Movie::CHILDRENS
-        result += 1.5
-        result += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
-      end
+    rental.charge
     end
 end
--- chapter1/Rental.rb
+++ chapter1/Rental.rb
@@ -1,5 +1,20 @@
 class Rental
   attr_reader :movie, :days_rented
+  def charge
+    result = 0
+    case movie.price_code
+      when Movie::REGULAR
+        result += 2
+        result += (days_rented - 2) * 1.5 if days_rented > 2
+      when Movie::NEW_RELEASE
+        result += days_rented * 3
+      when Movie::CHILDRENS
+        result += 1.5
+        result += (days_rented - 3) * 1.5 if days_rented > 3
+    end
+    result
+  end
+
   def initialize(movie, days_rented)
     @movie, @days_rented =movie, days_rented
   end
