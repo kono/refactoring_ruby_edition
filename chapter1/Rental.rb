6c74b455340ba7a564f849ae68e27f45ab26b4e9
Hiroshi Kono <hiroshi.kono@gmail.com>
2010-04-06 21:47:04 +0900

    p.43 moved adding frequent_renter_points from Customer class to Rental class

--- chapter1/Rental.rb
+++ chapter1/Rental.rb
@@ -14,6 +14,10 @@ class Rental
     end
     result
   end
+  
+  def frequent_renter_points
+    (movie.price_code == Movie::NEW_RELEASE && days_rented > 1) ? 2 : 1
+  end
 
   def initialize(movie, days_rented)
     @movie, @days_rented =movie, days_rented
