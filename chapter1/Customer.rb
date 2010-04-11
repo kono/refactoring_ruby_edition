b079fdf74e4ce41cd32ee15c269ff553949acfc8
Hiroshi Kono <hiroshi.kono@gmail.com>
2010-04-11 14:52:22 +0900

    p.55 added the function "html_statement" to Castomer class, and its test to TC_Customer class.

--- chapter1/Customer.rb
+++ chapter1/Customer.rb
@@ -27,6 +27,20 @@ class Customer
     print @result
   end
   
+  def html_statement
+    result ="<h1>Rentals for <em>#{@name}</em></h1><p>\n"
+    @rentals.each do |element|
+      # このレンタル料金を表示
+      result += "\t" + element.movie.title+": " + element.charge.to_s + "<br>\n"
+    end
+    # フッターを追加
+    result += "<p>You owe <em>#{total_charge}</em><p>\n"
+    result += "On this rental you earned " +
+      "<em>#{total_frequent_renter_points}</em> " +
+    "frequent renter points<p>"
+    result
+  end
+  
   private
   
   def total_charge
--- chapter1/TC_Customer.rb
+++ chapter1/TC_Customer.rb
@@ -29,4 +29,11 @@ class TC_Customer < Test::Unit::TestCase
     result= "Rental Record for kono\n\tStar Wars\t9.5\n\tDistrict 9\t6\n\tPonyo\t4.5\nAmount owed is 20.0\nYou earned 4 frequent renter points"
     assert_equal(result, @obj.result)
   end
+  must "test2" do
+    @obj.add_rental(Rental.new(@movie1, 10))
+    @obj.add_rental(Rental.new(@movie2, 15))
+    @obj.add_rental(Rental.new(@movie3, 20))
+    result="<h1>Rentals for <em>kono</em></h1><p>\n\tStar Wars: 14.0<br>\n\tDistrict 9: 45<br>\n\tPonyo: 27.0<br>\n<p>You owe <em>86.0</em><p>\nOn this rental you earned <em>4</em> frequent renter points<p>"
+    assert_equal(result, @obj.html_statement)
+  end
 end
