7e17d68a429d8e5b997e65352b76c6db65fe2ce6
Hiroshi Kono <hiroshi.kono@gmail.com>
2010-04-11 14:28:21 +0900

    p.51 use "inject" to calculate frequent_renter_points

--- chapter1/Customer.rb
+++ chapter1/Customer.rb
@@ -18,14 +18,12 @@ class Customer
     frequent_renter_points =  0
     @result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
-      frequent_renter_points += element.frequent_renter_points
-      
       # このレンタル料金を表示
       @result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
     end
     # フッター行を追加
     @result += "Amount owed is #{total_charge}\n"
-    @result += "You earned #{frequent_renter_points} frequent renter points"
+    @result += "You earned #{total_frequent_renter_points} frequent renter points"
     print @result
   end
   
@@ -35,4 +33,7 @@ class Customer
     @rentals.inject(0){|sum, rental| sum + rental.charge}
   end
   
+  def total_frequent_renter_points
+    @rentals.inject(0){|sum, rental| sum + rental.frequent_renter_points}
+  end
 end
