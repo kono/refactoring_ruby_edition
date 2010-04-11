cf4f2ed99013394a4e7b009c9decee23dc433a35
Hiroshi Kono <hiroshi.kono@gmail.com>
2010-04-11 16:07:48 +0900

    p.57 added the method "charge" to Movie class. chenged the method "charge" in Rental class.

--- chapter1/Rental.rb
+++ chapter1/Rental.rb
@@ -1,18 +1,7 @@
 class Rental
   attr_reader :movie, :days_rented
   def charge
-    result = 0
-    case movie.price_code
-      when Movie::REGULAR
-        result += 2
-        result += (days_rented - 2) * 1.5 if days_rented > 2
-      when Movie::NEW_RELEASE
-        result += days_rented * 3
-      when Movie::CHILDRENS
-        result += 1.5
-        result += (days_rented - 3) * 1.5 if days_rented > 3
-    end
-    result
+    movie.charge(days_rented)
   end
   
   def frequent_renter_points
