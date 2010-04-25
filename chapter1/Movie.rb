57c158021a8691141da3d7b904d32d2ff96fc30f
kono User <kono@ubuntu.ubuntu-domain>
2010-04-25 13:45:07 +0900

    p.63 revised

--- chapter1/Movie.rb
+++ chapter1/Movie.rb
@@ -11,12 +11,12 @@ class Movie
     @price = case price_code
     when REGULAR: RegularPrice.new
     when NEW_RELEASE: NewReleasePrice.new
-    when CHILDRENS: ChildrenPrice.new
+    when CHILDRENS: ChildrensPrice.new
     end
   end
   
-  def initialize(title, price_code)
-    @title, @price_code = title, price_code
+  def initialize(title, the_price_code)
+    @title, self.price_code = title, the_price_code
   end
   
   def charge(days_rented)
