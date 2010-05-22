13bdff4cc218d4c90301b4efb2b5d2a1fb17b2cc
Hiroshi Kono <hiroshi.kono@gmail.com>
2010-05-22 20:47:40 +0900

    p.140 replace temp "a_base_price" with query.

--- chapter6/Replace_Temp_with_Query.rb
+++ chapter6/Replace_Temp_with_Query.rb
@@ -7,13 +7,12 @@ class Replace_Temp_with_Query
     
 
   def price
-    a_base_price = base_price
-    if a_base_price >1000
+    if base_price >1000
       discount_factor = 0.95
     else
       discount_factor = 0.98
     end
-    a_base_price * discount_factor
+    base_price * discount_factor
   end
   
   def base_price
