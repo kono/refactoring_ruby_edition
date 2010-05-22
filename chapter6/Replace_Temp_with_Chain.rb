29b79a378ee623071fdd72a69e4c368dff8617c1
Hiroshi Kono <hiroshi.kono@gmail.com>
2010-05-22 21:20:01 +0900

    p.143 change method name "add_option" to "and".

--- chapter6/Replace_Temp_with_Chain.rb
+++ chapter6/Replace_Temp_with_Chain.rb
@@ -9,13 +9,13 @@ class Select
     @options ||=[]
   end
   
-  def add_option(arg)
+  def and(arg)
     options << arg
     self
   end
 end
 
 def main_proc
-  select = Select.with_option(1999).add_option(2000).add_option(2001).add_option(2002)
+  select = Select.with_option(1999).and(2000).and(2001).and(2002)
   select #=> #<select:0x28708 @options=[1999,2000,2001,2002]>
 end
