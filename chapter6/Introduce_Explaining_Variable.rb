15bb3b241e2b31e3eb0b96cba0db7b0abf4c7130
Hiroshi Kono <hiroshi.kono@gmail.com>
2010-05-23 08:22:14 +0900

    p.145 introduce temporary variable "quantity_discount" .

--- chapter6/Introduce_Explaining_Variable.rb
+++ chapter6/Introduce_Explaining_Variable.rb
@@ -8,8 +8,9 @@ class Introduce_Explaining_Variable
   def price
     # 価格は、基本価格 - 数量割引 + 配送料
     base_price = @quantity * @item_price
+    quantity_discount = [0, @quantity - 500].max * @item_price * 0.05
     return base_price -
-    [0, @quantity - 500].max * @item_price * 0.05 +
+    quantity_discount +
     [base_price * 0.1,100.0].min
   end
 end
