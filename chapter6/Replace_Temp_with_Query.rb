4f374a7f4afc30b54de707fc17860bc302d3e04e
Hiroshi Kono <hiroshi.kono@gmail.com>
2010-05-22 20:45:56 +0900

    p.139 extract method

--- chapter6/Replace_Temp_with_Query.rb
+++ chapter6/Replace_Temp_with_Query.rb
@@ -7,12 +7,16 @@ class Replace_Temp_with_Query
     
 
   def price
-    base_price = @quantity * @item_price
-    if base_price >1000
+    a_base_price = base_price
+    if a_base_price >1000
       discount_factor = 0.95
     else
       discount_factor = 0.98
     end
-    base_price * discount_factor
+    a_base_price * discount_factor
+  end
+  
+  def base_price
+    @quantity * @item_price
   end
 end
