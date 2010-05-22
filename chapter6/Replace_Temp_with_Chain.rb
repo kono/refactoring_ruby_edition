90db8660c34ffcbe53f0b720f7bdbc2e48b60436
Hiroshi Kono <hiroshi.kono@gmail.com>
2010-05-22 21:18:28 +0900

    p.142 change add_option method in order to chain-able.

--- chapter6/Replace_Temp_with_Chain.rb
+++ chapter6/Replace_Temp_with_Chain.rb
@@ -11,13 +11,11 @@ class Select
   
   def add_option(arg)
     options << arg
+    self
   end
 end
 
 def main_proc
-  select = Select.with_option(1999)
-  select.add_option(2000)
-  select.add_option(2001)
-  select.add_option(2002)
+  select = Select.with_option(1999).add_option(2000).add_option(2001).add_option(2002)
   select #=> #<select:0x28708 @options=[1999,2000,2001,2002]>
 end
