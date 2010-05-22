a2c35b2bfc1faf18787c1eddbe6ef1951fec5cd1
Hiroshi Kono <hiroshi.kono@gmail.com>
2010-05-23 08:26:30 +0900

    p.146 extract method "base_price", "quantity_discount", and "shipping".

--- chapter6/Introduce_Explaining_Variable.rb
+++ chapter6/Introduce_Explaining_Variable.rb
@@ -7,10 +7,19 @@ class Introduce_Explaining_Variable
   
   def price
     # 価格は、基本価格 - 数量割引 + 配送料
-    base_price = @quantity * @item_price
-    quantity_discount = [0, @quantity - 500].max * @item_price * 0.05
-    shipping = [base_price * 0.1, 100.0].min
-    return base_price - quantity_discount + shipping
+    base_price - quantity_discount + shipping
+  end
+  
+  def base_price
+    @quantity * @item_price
+  end
+  
+  def quantity_discount
+    [0, @quantity - 500].max * @item_price * 0.05
+  end
+  
+  def shipping
+    [base_price * 0.1, 100.0].min
   end
 end
 
