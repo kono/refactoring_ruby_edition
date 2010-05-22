f70b45cd7102e0d06c370c20a39b2a1c508faf2f
Hiroshi Kono <hiroshi.kono@gmail.com>
2010-05-23 08:23:51 +0900

    p.146 introduce temporary variable "shipping" .

--- chapter6/Introduce_Explaining_Variable.rb
+++ chapter6/Introduce_Explaining_Variable.rb
@@ -9,9 +9,8 @@ class Introduce_Explaining_Variable
     # 価格は、基本価格 - 数量割引 + 配送料
     base_price = @quantity * @item_price
     quantity_discount = [0, @quantity - 500].max * @item_price * 0.05
-    return base_price -
-    quantity_discount +
-    [base_price * 0.1,100.0].min
+    shipping = [base_price * 0.1, 100.0].min
+    return base_price - quantity_discount + shipping
   end
 end
 
