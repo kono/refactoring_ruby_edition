ddcfcf9a0ae072351aabc735d5f87194400e50a9
Hiroshi Kono <hiroshi.kono@gmail.com>
2010-05-22 20:50:05 +0900

    p.140 extract method "discount_factor".

--- chapter6/Replace_Temp_with_Query.rb
+++ chapter6/Replace_Temp_with_Query.rb
@@ -7,15 +7,15 @@ class Replace_Temp_with_Query
     
 
   def price
-    if base_price >1000
-      discount_factor = 0.95
-    else
-      discount_factor = 0.98
-    end
-    base_price * discount_factor
+    a_discount_factor = discount_factor
+    base_price * a_discount_factor
   end
   
   def base_price
     @quantity * @item_price
+  end
+  
+  def discount_factor
+    base_price > 1000 ? 0.95 : 0.98
   end
 end
