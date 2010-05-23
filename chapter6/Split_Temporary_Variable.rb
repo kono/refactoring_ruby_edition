73853fe4bf0ac3f5fe6e11cedbc6425cac66b9a6
Hiroshi Kono <hiroshi.kono@gmail.com>
2010-05-23 09:18:19 +0900

    p.149 extract method "primary_acc", "primary_vel", "secondary_acc".

--- chapter6/Split_Temporary_Variable.rb
+++ chapter6/Split_Temporary_Variable.rb
@@ -7,16 +7,27 @@ class Haggis
   end
 
   def distance_traveled(time)
-    primary_acc = @primary_force / @mass
     primary_time = [time, @delay].min
     result = 0.5 * primary_acc * primary_time * primary_time
+    
     secondary_time = time - @delay
     if (secondary_time > 0)
-      primary_vel = primary_acc * @delay
-      secondary_acc = (@primary_force + @secondary_force) / @mass
       result += primary_vel * secondary_time + 5 * secondary_acc * secondary_time * secondary_time
     end
+    
     result
+  end
+  
+  def primary_acc
+    @primary_force / @mass
+  end
+  
+  def primary_vel
+    primary_acc * @delay
+  end
+  
+  def secondary_acc
+    (@primary_force + @secondary_force) / @mass
   end
 end
 
