c5c89d2757898ba1cd1d94512c0253856fcd7edf
Hiroshi Kono <hiroshi.kono@gmail.com>
2010-05-30 09:05:34 +0900

    p.155-156 replace "gamma" method with "compute" method in "Gamma" class.

--- chapter6/Replace_Method_with_Method_Object.rb
+++ chapter6/Replace_Method_with_Method_Object.rb
@@ -1,16 +1,39 @@
 class Account
   def gamma(input_val, quantity, year_to_date)
-    important_value1 = (input_val * quantity + delta)
-    important_value2 = (input_val * year_to_date) + 100
-    if (year_to_date - important_value1) > 100
-      important_value2 -= 20
-    end
-    important_value3 = important_value2 * 7
-    #等々
-    important_value3 - 2 * important_value1
+    Gamma.new(self, input_val, quantity, year_to_date).compute
   end
   
   def delta
     return 100
   end
 end
+
+class Gamma
+  attr_reader :account,
+              :imput_val,
+              :quantity,
+              :year_to_date,
+              :important_value1,
+              :important_value2,
+              :important_value3
+  
+  def initialize(account, input_val_arg, quantity_arg, year_to_date_arg)
+    @account = account
+    @input_val = input_val_arg
+    @quantity = quantity_arg
+    @year_to_date = year_to_date_arg
+  end
+  
+  def compute
+    @important_value1 = (@input_val * @quantity) + @account.delta
+    @important_value2 = (@input_val * @year_to_date) + 100
+    if (year_to_date - important_value1) > 100
+      @important_value2 -= 20
+    end
+    @important_value3 = @important_value2 * 7
+    # and so on.
+    @important_value3 - 2 * @important_value1
+  end
+
+end
+
